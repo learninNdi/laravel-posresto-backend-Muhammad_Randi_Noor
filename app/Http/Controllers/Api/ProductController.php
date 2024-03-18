34a61505ac3ebf6fcc0d6cd647a50635ab5c83d4
Muhammad Randi Noor <noorrandim@gmail.com>
2024-03-18 14:41:31 +0700

    change load category in ProductController method

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -16,41 +17,48 @@ class ProductController extends Controller
         // $products = Product::paginate(10);
         $products = Product::all();
 
+        // load the category
+        $products->load('category');
+
+        // foreach ($products as $product) {
+        //     $product->category_name = Category::find($product->category_id)->name;
+        // }
+
         return response()->json([
             'status' => 'success',
             'data' => $products,
         ], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    // /**
+    //  * Store a newly created resource in storage.
+    //  */
+    // public function store(Request $request)
+    // {
+    //     //
+    // }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Product $product)
-    {
-        //
-    }
+    // /**
+    //  * Display the specified resource.
+    //  */
+    // public function show(Product $product)
+    // {
+    //     //
+    // }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Product $product)
-    {
-        //
-    }
+    // /**
+    //  * Update the specified resource in storage.
+    //  */
+    // public function update(Request $request, Product $product)
+    // {
+    //     //
+    // }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Product $product)
-    {
-        //
-    }
+    // /**
+    //  * Remove the specified resource from storage.
+    //  */
+    // public function destroy(Product $product)
+    // {
+    //     //
+    // }
 }
